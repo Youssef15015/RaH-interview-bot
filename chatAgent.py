2a86818c10c1dab07666dd5a2cc174d4b6af3bdd
Youssef15015 <133540204+Youssef15015@users.noreply.github.com>
2025-01-12 17:43:52 -0600

    Initial Commit

---
+++ chatAgent.py
@@ -0,0 +1,49 @@
+import streamlit as st
+import os
+import pickle
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_nvidia_ai_endpoints import ChatNVIDIA
+
+
+if not os.getenv("NIVIDA_API_KEY"):
+    os.environ["NVIDIA_API_KEY"] = "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"
+
+
+st.set_page_config(layout="wide")
+llm = ChatNVIDIA(mode = "mixtral_8x7b")
+
+st.subheader("Chat with your AI Assistant, Interview Bot!")
+
+vector_store_path = "vectorstore.pkl"
+with open(vector_store_path, "rb") as f:
+    vectorstore = pickle.load(f)
+
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+
+prompt_template = ChatPromptTemplate.from_messages(
+    [("system", "You are a helpful AI assistant named Interview Bot..."), ("user", "{input}")]
+)
+chain = prompt_template | llm | StrOutputParser()
+
+
+user_input = st.chat_input("Ask your question:")
+if user_input and vectorstore is not None:
+    st.session_state.messages.append({"role": "user", "content": user_input})
+
+    retriever = vectorstore.as_retriever()
+    context = "\n\n".join(doc.page_content for doc in retriever.get_relevant_documents(user_input))
+    augmented_user_input = f"Context: {context}\n\nQuestion: {user_input}"
+
+
+    response = chain.invoke({"input": augmented_user_input})
+    st.session_state.messages.append({"role": "assistant", "content": response})
+    with st.chat_message("assistant"):
+        st.markdown(response)
---
+++ vector_store.py
@@ -0,0 +1,60 @@
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.document_loaders import DirectoryLoader
+from langchain.vectorstores import FAISS
+from langchain_nvidia_ai_endpoints import ChatNVIDIA, NVIDIAEmbeddings
+import pickle
+import streamlit as st
+import os
+import getpass
+
+
+if not os.getenv("NIVIDA_API_KEY"):
+    os.environ["NVIDIA_API_KEY"] = "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"
+
+
+st.set_page_config(layout="wide")
+llm = ChatNVIDIA(mode = "mixtral_8x7b")
+document_embedder = NVIDIAEmbeddings(model="NV-Embed-QA", mode_type="passage")
+# query_embedder = NVIDIAEmbeddings(mode="embed-qa-4", model_type="query")
+
+
+with st.sidebar:
+    DOCS_DIR = os.path.abspath("./uploaded_docs")
+    if not os.path.exists(DOCS_DIR):
+        os.makedirs(DOCS_DIR)
+    st.subheader("Add to the Knowledge Base")
+    with st.form("my-form", clear_on_submit=True):
+        uploaded_files = st.file_uploader("Upload a file to the Knowledge Base:",
+accept_multiple_files=True)
+        submitted = st.form_submit_button("Upload!")
+
+
+    if uploaded_files and submitted:
+        for uploaded_file in uploaded_files:
+            st.success(f"File {uploaded_file.name} uploaded successfully!")
+            with open (os.path.join(DOCS_DIR, uploaded_file.name), "wb") as f:
+                f.write(uploaded_file.read())
+
+
+use_existing_vector_store = st.radio("Use existing vector store",["Yes", "No"])
+
+vector_store_path = "vectorstore.pkl"
+
+raw_documents = DirectoryLoader(DOCS_DIR).load()
+
+vector_store_exists = os.path.exists(vector_store_path)
+vectorstore = None
+
+if use_existing_vector_store == "Yes" and vector_store_exists:
+    with open(vector_store_path, "rb") as f:
+        vectorstore = pickle.load(f)
+
+else:
+    if raw_documents:
+        text_splitter = CharacterTextSplitter(chunk_size = 2000) 
+        documents = text_splitter.split_documents(raw_documents)
+
+        vectorstore = FAISS.from_documents(documents, document_embedder)
+
+        with open(vector_store_path, "wb") as f:
+            pickle.dump(vectorstore, f)
