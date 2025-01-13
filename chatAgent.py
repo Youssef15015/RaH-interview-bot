3c810fd74129a3650fe48967d45b35bbff2d0101
Youssef15015 <133540204+Youssef15015@users.noreply.github.com>
2025-01-12 21:13:43 -0600

    Update chatAgent.py

--- chatAgent.py
+++ chatAgent.py
@@ -9,7 +9,7 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain_nvidia_ai_endpoints import ChatNVIDIA, NVIDIAEmbeddings
 
 
-if not os.getenv("NIVIDA_API_KEY"):
+if not os.getenv("NVIDIA_API_KEY"):
     os.environ["NVIDIA_API_KEY"] = NVIDIA_API_KEY
 
 
